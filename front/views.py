5bd37cd05af84a02145056038e99d24a5d53adb1
Armun Alam <armunalam@gmail.com>
2021-08-11 08:07:51 +0600

    add provisional login page

--- front/views.py
+++ front/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'front/index.html')
+
+
+def loginpage(request):
+    return render(request, 'front/login-dark-white.html')
